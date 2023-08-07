5458fafc9d101706a54df56074ac992c6e0abba8
Suzanne <suzanneschuber@gmail.com>
2023-08-07 16:09:00 -0700

    Update controllers with constructors Co-Authored By: Jason Church elijahchurch@gmail.com

--- RecipeBox/Controllers/IngsController.cs
+++ RecipeBox/Controllers/IngsController.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace RecipeBox.Controllers
 {
--- RecipeBox/Controllers/RecipesController.cs
+++ RecipeBox/Controllers/RecipesController.cs
@@ -5,12 +5,21 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace RecipeBox.Controllers
 {
     [Authorize]
     public class RecipesController : Controller
     {
-        
+        private readonly RecipeBoxContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public RecipesController(UserManager<ApplicationUser> userManager, RecipeBoxContext db) 
+        {
+            _userManager =userManager;
+            _db = db;
+        }
     }
 }
